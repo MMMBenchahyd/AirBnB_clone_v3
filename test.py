851538975aed61140bc5cd9b40e6da4ece46fcb2
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-03-30 09:35:56 +0000

    task_2

--- test.py
+++ test.py
@@ -2,9 +2,10 @@
 """
 from models import storage
 from models.state import State
+from models.user import User
 
-# print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
-
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+print("All objects: {}".format(storage.count()))
+print("State objects: {}".format(storage.count('Use')))
+ 
+first_state_id = list(storage.all(User).values())[0].id
+print("First state: {}".format(storage.get(User, first_state_id)))
